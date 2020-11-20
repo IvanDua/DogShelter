d76170017d15b92d8203a00b916c98bd1b10837e
IvanDua <74757254+IvanDua@users.noreply.github.com>
2020-11-19 22:51:44 -0700

    Add files via upload

---
+++ 412_Project.py
@@ -0,0 +1,71 @@
+from subprocess import call
+import os
+import shutil
+import psycopg2
+import sys
+import loadData as loadHelper #import all load statements
+
+DB_NAME = 'project_412'
+
+
+def getOpenConnection(user='postgres', password='1234', dbname='Project_412'):
+
+    return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
+
+#################################################################################################################################################
+
+
+def startUpDB(dbname):
+
+    # Connect to defualt Postgres DB
+
+    connection = getOpenConnection(dbname='postgres')
+    connection.set_isolation_level(
+        psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    cursor = connection.cursor()
+
+    # Select from existing databases to see if database was created previously.
+
+    cursor.execute(
+        'SELECT COUNT(*) FROM pg_catalog.pg_database WHERE datname = \'' + dbname + '\'')
+
+    occurrences = cursor.fetchone()[0]
+
+    if occurrences == 0:
+        cursor.execute('CREATE DATABASE ' + dbname)
+
+        connection2 = getOpenConnection(dbname=DB_NAME)
+        ##################################   Load Tables   ##################################
+        loadHelper.loadDogTable('dogs', 'dogs_data.txt', connection2)
+        loadHelper.loadShelterTable('shelters', 'shelter_data.txt', connection2)
+        loadHelper.loadPersonTable('person','person.txt', connection2)
+        loadHelper.loadEmployeeTable('employees', 'employee.txt', connection2)
+        loadHelper.loadAdopterTable('adopters', 'adopter.txt', connection2) # Needs additional columns, simply create empty table and populate during runtime
+        loadHelper.loadPetAdoptionTable('pet_adoption', 'pet_adoptions.txt', connection2)
+        loadHelper.loadAppliesForTable('applies_for' , 'applies_for.txt' , connection2)
+        #cursor2 = connection2.cursor()
+        #cursor2.execute(
+        #    "CREATE TABLE pet_adoption (adopterID int, employeeID int, dogID text, date text, contractID int);")
+
+    else:
+        print('Database was created previously.')
+
+    connection.commit()
+    connection2.commit()
+    cursor.close()
+    #cursor2.close()
+    connection.close()
+
+#################################################################################################################################################
+
+
+def main():
+
+    startUpDB(DB_NAME)
+
+#################################################################################################################################################
+
+
+if __name__ == '__main__':
+    main()
---
+++ loadData.py
@@ -0,0 +1,209 @@
+#################################################################################################################################################
+
+
+def loadPersonTable(tableName, filePath, openConnection):
+
+    cursor = openConnection.cursor()
+    
+    # Be sure to add primary and foreign keys later!!!!!!!!!!!!!
+    cursor.execute("CREATE TABLE " + tableName + " (name text, personID int, PRIMARY KEY (personID));")
+
+    fp = open(filePath, "r")
+
+    dataList = fp.readlines()
+    
+    for i in dataList:
+        splitList = i.split('::')
+
+        name = splitList[0]
+        personID = int(splitList[1])
+
+        cursor.execute("INSERT INTO " + tableName + " VALUES (%s, %s)", (name, personID))
+
+    fp.close()
+
+    cursor.close()
+    openConnection.commit()
+
+
+#################################################################################################################################################
+
+
+def loadEmployeeTable(tableName, filePath, openConnection):
+
+    cursor = openConnection.cursor()
+    
+    # Be sure to add primary and foreign keys later!!!!!!!!!!!!!
+    cursor.execute("CREATE TABLE " + tableName + " (name text, personID int, shelterID int, PRIMARY KEY (personID));") 
+
+    fp = open(filePath, "r")
+
+    dataList = fp.readlines()
+    
+    for i in dataList:
+        splitList = i.split('::')
+
+        name = splitList[0]
+        personID = int(splitList[1])
+        shelterID = int(splitList[2])
+
+        cursor.execute("INSERT INTO " + tableName + " VALUES (%s, %s, %s )", (name, personID, shelterID))
+
+    fp.close()
+
+    cursor.close()
+    openConnection.commit()
+
+#################################################################################################################################################
+
+
+def loadAdopterTable(tableName, filePath, openConnection):
+
+    cursor = openConnection.cursor()
+    
+    # Be sure to add primary and foreign keys later!!!!!!!!!!!!!
+    cursor.execute("CREATE TABLE " + tableName + " (name text, personID int, zipCode int, phoneNumber text, PRIMARY KEY (personID));") 
+
+    fp = open(filePath, "r")
+
+    dataList = fp.readlines()
+    
+    for i in dataList:
+        splitList = i.split('::')
+
+        name = splitList[0]
+        personID = int(splitList[1])
+        zipCode = int(splitList[2])
+        phoneNumber = splitList[3]
+
+        cursor.execute("INSERT INTO " + tableName + " VALUES (%s, %s, %s , %s)", (name, personID, zipCode, phoneNumber))
+
+    fp.close()
+
+    cursor.close()
+    openConnection.commit()
+
+#################################################################################################################################################
+
+def loadDogTable(tableName, filePath, openConnection):
+
+    cursor = openConnection.cursor()
+    
+    # Be sure to add primary and foreign keys later!!!!!!!!!!!!!
+    cursor.execute("CREATE TABLE " + tableName + " (breed text, intakeType text, sex text, maintenanceLevel text, temperament text, age int, adoptionStatus text, shelterID int  , dogID int, PRIMARY KEY (dogID));") 
+
+    dogFile = open(filePath, "r")
+
+    dataList = dogFile.readlines()
+    
+    for i in dataList:
+        splitList = i.split('::')
+
+        breed = splitList[0]
+        intakeType = splitList[1]
+        sex = splitList[2]
+        maintenanceLevel = splitList[3]
+        temperament = splitList[4]
+        age = int(splitList[5])
+        adoptionStatus = splitList[6]
+        shelterID = int(splitList[7])
+        dogID = int(splitList[8])
+
+        cursor.execute("INSERT INTO " + tableName + " VALUES (%s, %s, %s , %s , %s , %s, %s, %s , %s)", (breed, intakeType, sex, maintenanceLevel, temperament, age, adoptionStatus, shelterID,dogID))
+
+    dogFile.close()
+
+    cursor.close()
+    openConnection.commit()
+
+#################################################################################################################################################
+
+
+def loadShelterTable(tableName, filePath, openConnection):
+
+    cursor = openConnection.cursor()
+    
+    # Be sure to add primary and foreign keys later!!!!!!!!!!!!!
+    cursor.execute("CREATE TABLE " + tableName + " (name text, phoneNumber text, address text, shelterID int, PRIMARY KEY (shelterID));") 
+
+    fp = open(filePath, "r")
+
+    dataList = fp.readlines()
+    
+    for i in dataList:
+        splitList = i.split('::')
+
+        name = splitList[0]
+        number = splitList[1]
+        address = splitList[2]
+        shelterID = int(splitList[3])
+
+        cursor.execute("INSERT INTO " + tableName + " VALUES (%s, %s, %s , %s)", (name, number, address, shelterID))
+
+    fp.close()
+
+    cursor.close()
+    openConnection.commit()
+
+#################################################################################################################################################
+def loadPetAdoptionTable(tableName, filePath, openConnection):
+
+
+    cursor = openConnection.cursor()
+    
+    # Be sure to add primary and foreign keys later!!!!!!!!!!!!!
+    cursor.execute("CREATE TABLE " + tableName + " (adopterID int, employeeID int, dogID int, date text, contractID int, PRIMARY KEY (contractID));") 
+
+    '''
+    fp = open(filePath, "r")
+
+    dataList = fp.readlines()
+    
+    for i in dataList:
+        splitList = i.split('::')
+
+        contractDate = splitList[0]
+        contractID = splitList[1]
+       
+
+        cursor.execute("INSERT INTO " + tableName + " VALUES (%s, %s)", (contractDate,contractID))
+
+    fp.close()
+    '''
+
+    cursor.close()
+    openConnection.commit()
+
+#################################################################################################################################################
+
+def loadAppliesForTable(tableName, filePath, openConnection):
+
+
+    cursor = openConnection.cursor()
+    
+    # Be sure to add primary and foreign keys later!!!!!!!!!!!!!
+    cursor.execute("CREATE TABLE " + tableName + " ( desiredBreed text, desiredSex text , desiredTemperament text, maxMaintenanceLevel text, desiredIntakeType text , maxAge int , personID text);") 
+
+    
+    fp = open(filePath, "r")
+
+    dataList = fp.readlines()
+    
+    for i in dataList:
+        splitList = i.split('::')
+
+        desiredBreed = splitList[0]
+        desiredSex = splitList[1]
+        desiredTemperament = splitList[2]
+        maxMaintenanceLevel = splitList[3]
+        desiredIntakeType = splitList[4]
+        maxAge = splitList[5]
+        personID = splitList[6]
+
+        cursor.execute("INSERT INTO " + tableName + " VALUES (%s, %s , %s , %s , %s , %s, %s)", (desiredBreed,desiredSex, desiredTemperament, maxMaintenanceLevel, desiredIntakeType, maxAge, personID))
+
+    fp.close()
+    
+
+    cursor.close()
+    openConnection.commit()
